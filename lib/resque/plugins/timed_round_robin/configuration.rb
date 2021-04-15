e18b33094904d0297a6504ef7d6b0546b4ca8dc7
Josh Rickard <josh.rickard@gmail.com>
2021-04-15 13:21:26 -0500

    Add support for paused queues

--- lib/resque/plugins/timed_round_robin/configuration.rb
+++ lib/resque/plugins/timed_round_robin/configuration.rb
@@ -1,9 +1,10 @@
 module Resque::Plugins
   module TimedRoundRobin
     class Configuration
-      attr_accessor :queue_depths, :queue_refresh_interval
+      attr_accessor :paused_queues_set, :queue_depths, :queue_refresh_interval
 
       def initialize
+        @paused_queues_set = nil
         @queue_depths = {}
         @queue_refresh_interval = 60
       end
