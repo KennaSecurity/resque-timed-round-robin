8d88bfe7ea28370756427f8484e610d324b14b9e
Jason Garber <122489457+jgarber-cisco@users.noreply.github.com>
2024-04-02 10:04:44 -0400

    Switch to standard redis port 6379 everywhere

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,9 +6,9 @@ require 'timecop'
 spec_dir = File.dirname(File.expand_path(__FILE__))
 REDIS_CMD = "redis-server #{spec_dir}/redis-test.conf"
 
-puts "Starting redis for testing at localhost:9736..."
+puts "Starting redis for testing at localhost:6379..."
 puts `cd #{spec_dir}; #{REDIS_CMD}`
-Resque.redis = 'localhost:9736'
+Resque.redis = 'localhost:6379'
 
 # Schedule the redis server for shutdown when tests are all finished.
 at_exit do
